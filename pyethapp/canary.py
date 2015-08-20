504b0f4d6c201e799c1aa8e404cf77e4df483f27
Heiko Heiko <heiko@heiko.org>
2015-08-20 18:14:16 +0200

    updated canary addresses

--- pyethapp/canary.py
+++ pyethapp/canary.py
@@ -1,8 +1,8 @@
 import ethereum.utils as utils
 
 canary_addresses = map(utils.decode_hex, [
-    'e3e942b2aa524293c84ff6c7f87a6635790ad5e4',  # Gav
-    'b1e570be07eaa673e4fd0c8265b64ef739385709',  # Vitalik
-    '9d38997c624a71b21278389ea2fdc460d000e4b2',  # Jeff
-    '529bc43a5d93789fa28de1961db6a07e752204ae'   # Christoph
+    '539dd9aaf45c3feb03f9c004f4098bd3268fef6b',  # Gav
+    'c8158da0b567a8cc898991c2c2a073af67dc03a9',  # Vitalik
+    '959c33de5961820567930eccce51ea715c496f85',  # Jeff
+    '7a19a893f91d5b6e2cdf941b6acbba2cbcf431ee'   # Christoph
 ])
