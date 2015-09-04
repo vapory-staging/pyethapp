4f28065eee76060b9ba9c22e3cf1298bb02c4415
Heiko Heiko <heiko@heiko.org>
2015-09-04 12:46:58 +0200

    wip: add support for olympics

--- pyethapp/profiles.py
+++ pyethapp/profiles.py
@@ -1,5 +1,5 @@
 from os import path
-
+from ethereum.utils import denoms
 DEFAULT_PROFILE = 'frontier'
 
 PROFILES = {
@@ -20,3 +20,22 @@ PROFILES = {
         }
     },
 }
+
+
+olympic_params = dict(
+    MIN_GAS_LIMIT=125000,
+    GASLIMIT_EMA_FACTOR=1024,
+    GASLIMIT_ADJMAX_FACTOR=1024,
+    BLKLIM_FACTOR_NOM=3,
+    BLKLIM_FACTOR_DEN=2,
+    BLOCK_REWARD=1500 * denoms.finney,
+    UNCLE_DEPTH_PENALTY_FACTOR=8,
+    NEPHEW_REWARD=(1500 * denoms.finney) // 32,
+    MAX_UNCLE_DEPTH=6,
+    MAX_UNCLES=2,
+    DIFF_ADJUSTMENT_CUTOFF=8,
+    BLOCK_DIFF_FACTOR=2048,
+    MIN_DIFF=131072,
+    POW_EPOCH_LENGTH=30000)
+
+PROFILES['olympic']['eth']['block'] = olympic_params
--- pyethapp/tests/test_genesis.py
+++ pyethapp/tests/test_genesis.py
@@ -1,3 +1,4 @@
+import pytest
 from ethereum import blocks
 from ethereum.db import DB
 from ethereum.config import Env
@@ -7,28 +8,35 @@ import pyethapp.config as konfig
 from pyethapp.profiles import PROFILES
 
 
-def test_genesis():
-    for profile in ['frontier']:  # fixme olympics
+def check_genesis(profile):
+    config = dict(eth=dict())
 
-        config = dict(eth=dict())
+    # Set config values based on profile selection
+    merge_dict(config, PROFILES[profile])
 
-        # Set config values based on profile selection
-        merge_dict(config, PROFILES[profile])
+    # Load genesis config
+    update_config_from_genesis_json(config, config['eth']['genesis'])
 
-        # Load genesis config
-        update_config_from_genesis_json(config, config['eth']['genesis'])
+    konfig.update_config_with_defaults(config, {'eth': {'block': blocks.default_config}})
 
-        konfig.update_config_with_defaults(config, {'eth': {'block': blocks.default_config}})
+    print config['eth'].keys()
+    bc = config['eth']['block']
+    print bc.keys()
+    env = Env(DB(), bc)
 
-        print config['eth'].keys()
-        bc = config['eth']['block']
-        print bc.keys()
-        env = Env(DB(), bc)
+    genesis = blocks.genesis(env)
+    print 'genesis.hash', genesis.hash.encode('hex')
+    print 'expected', config['eth']['genesis_hash']
+    assert genesis.hash == config['eth']['genesis_hash'].decode('hex')
 
-        genesis = blocks.genesis(env)
-        print 'genesis.hash', genesis.hash.encode('hex')
-        print 'expected', config['eth']['genesis_hash']
-        assert genesis.hash == config['eth']['genesis_hash'].decode('hex')
+
+@pytest.mark.xfail  # FIXME
+def test_olympic():
+    check_genesis('olympic')
+
+
+def test_frontier():
+    check_genesis('frontier')
 
 
 if __name__ == '__main__':
