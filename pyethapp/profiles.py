9349d6ed31acd17544fb0ad21dfcb96d1a0252d9
Heiko Heiko <heiko@heiko.org>
2015-09-04 08:07:56 +0200

    abs path and genesis_hash check value in profiles

--- pyethapp/profiles.py
+++ pyethapp/profiles.py
@@ -6,13 +6,17 @@ PROFILES = {
     'frontier': {
         'eth': {
             'network_id': 1,
-            'genesis': path.join(path.dirname(__file__), 'data', 'genesis_frontier.json'),
+            'genesis': path.abspath(path.join(path.dirname(__file__), '..',
+                                              'data', 'genesis_frontier.json')),
+            'genesis_hash': 'd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3'
         }
     },
     'olympic': {
         'eth': {
             'network_id': 0,
-            'genesis': path.join(path.dirname(__file__), 'data', 'genesis_olympic.json'),
+            'genesis': path.abspath(path.join(path.dirname(__file__), '..',
+                                              'data', 'genesis_olympic.json')),
+            'genesis_hash': 'fd4af92a79c7fc2fd8bf0d342f2e832e1d4f485c85b9152d2039e03bc604fdca'
         }
     },
 }
