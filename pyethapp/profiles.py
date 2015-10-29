7a41ecc1b02a15e7ef381e76129c2624658e1f1a
Ulrich Petri <ulo@ulo.pe>
2015-10-29 18:15:00 +0100

    Fix nesting error

--- pyethapp/profiles.py
+++ pyethapp/profiles.py
@@ -18,15 +18,19 @@ PROFILES = {
         'eth': {
             'network_id': 2,
             'genesis': path.join(genesisdata_dir, 'genesis_morden.json'),
-        'genesis_hash': '0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303',
-        'block': {
-            'ACCOUNT_INITIAL_NONCE': 2 ** 20
-        }
-    },
-    'discovery': {
-        'bootstrap_nodes': [
-            'enode://e58d5e26b3b630496ec640f2530f3e7fa8a8c7dfe79d9e9c4aac80e3730132b869c852d3125204ab35bb1b1951f6f2d40996c1034fd8c5a69b383ee337f02ddc@92.51.165.126:30303'
-        ]
+            'genesis_hash': '0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303',
+            'block': {
+                'ACCOUNT_INITIAL_NONCE': 2 ** 20
+            },
+            'discovery': {
+                'bootstrap_nodes': [
+                    (
+                        'enode://e58d5e26b3b630496ec640f2530f3e7fa8a8c7dfe79d9e9c4aac80e3730132b8'
+                        '69c852d3125204ab35bb1b1951f6f2d40996c1034fd8c5a69b383ee337f02ddc'
+                        '@92.51.165.126:30303'
+                    )
+                ]
+            }
+        },
     }
-}
 }
