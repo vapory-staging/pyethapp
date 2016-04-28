cd0aef920ce537287af0f8949fde03c0d97c019b
Ulrich Petri <ulo@ulo.pe>
2016-04-28 18:47:41 +0200

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ test_requirements = ['ethereum-serpent>=1.8.1', 'pytest']
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '1.2.0'
+version = '1.2.1'
 
 setup(
     name='pyethapp',
