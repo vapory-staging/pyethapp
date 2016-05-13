34686396a933827a003d9487d54739a7947fcfd8
Ulrich Petri <ulo@ulo.pe>
2016-05-13 19:28:02 +0200

    Bump version: 1.2.1 → 1.2.2

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ test_requirements = [
 
 # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
 # see: https://github.com/ethereum/pyethapp/wiki/Development:-Versions-and-Releases
-version = '1.2.1'
+version = '1.2.2'
 
 setup(
     name='pyethapp',
