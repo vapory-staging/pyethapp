2f086d2634595cf081c00c9794553027de272e99
Konrad Feldmeier <konrad@brainbot.com>
2016-06-08 10:59:32 +0200

    Remove TEST from notification

--- .release_notify.sh
+++ .release_notify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-PAYLOAD="{\"text\": \"TEST: <https://pypi.python.org/pypi/pyethapp|pyethapp $TRAVIS_TAG> was released on pypi!\"}"
+PAYLOAD="{\"text\": \"<https://pypi.python.org/pypi/pyethapp|pyethapp $TRAVIS_TAG> was released on pypi!\"}"
 curl -s -X POST --data-urlencode "payload=$PAYLOAD" $SLACK_WEBHOOK_URL
