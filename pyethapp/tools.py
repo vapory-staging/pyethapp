3a412a020bb6e6545635e5ac27d180a1f34a48e4
Jan Xie <jan.h.xie@gmail.com>
2016-09-07 13:53:35 +0800

    update tool to generate datadir for validators

--- pyethapp/tools.py
+++ pyethapp/tools.py
@@ -1,12 +1,70 @@
+import os
 import sys
 import time
 import json
+import yaml
 
 from ethereum import utils
 from ethereum.state_transition import apply_const_message
 from ethereum.casper_utils import RandaoManager, generate_validation_code, make_casper_genesis, casper_config, call_casper
+from devp2p.crypto import privtopub
 
-def generate_genesis(path, num_participants=1):
+def generate_data_dirs(num_participants, prefix='v'):
+    privkeys = [utils.sha3(str(i)) for i in range(num_participants)]
+    addrs = [utils.privtoaddr(k) for k in privkeys]
+    genesis = generate_genesis(None, num_participants)
+
+    for i in range(num_participants):
+        privkey = privkeys[i]
+        addr = addrs[i]
+        port = 40000+i
+        jsonrpc_port = 4000+i
+
+        bootstrap_nodes = range(num_participants)
+        bootstrap_nodes.remove(i)
+        bootstrap_nodes = ["enode://%s@0.0.0.0:%d" % (utils.encode_hex(privtopub(privkeys[n])), 40000+n) for n in bootstrap_nodes]
+
+        dir = prefix + str(i)
+        try:
+            os.stat(dir)
+        except:
+            os.mkdir(dir)
+        genesis_path = dir + '/genesis.json'
+        config_path = dir + '/config.yaml'
+
+        config = {
+            "node": {
+                "privkey_hex": utils.encode_hex(privkey)
+            },
+            "eth": {
+                "genesis": genesis_path
+            },
+            "p2p": {
+                "num_peers": num_participants-1,
+                "listen_host": '0.0.0.0',
+                "listen_port": port
+            },
+            "discovery": {
+                "listen_host": '0.0.0.0',
+                "listen_port": port,
+                "bootstrap_nodes": bootstrap_nodes
+            },
+            "jsonrpc": {
+                "listen_host": '0.0.0.0',
+                "listen_port": jsonrpc_port
+            }
+        }
+
+        with open(genesis_path, 'w') as f:
+            json.dump(genesis, f, sort_keys=False, indent=4, separators=(',', ': '))
+        print "genesis for validator %d generated" % i
+
+        with open(config_path, 'w') as f:
+            yaml.dump(config, f, default_flow_style=False, indent=4)
+        print "config for validator %d generated" % i
+
+
+def generate_genesis(path=None, num_participants=1):
     privkeys = [utils.sha3(str(i)) for i in range(num_participants)]
     addrs = [utils.privtoaddr(k) for k in privkeys]
     deposit_sizes = [i * 500 + 500 for i in range(num_participants)]
@@ -39,16 +97,30 @@ def generate_genesis(path, num_participants=1):
         "gasLimit": utils.int_to_hex(header.gas_limit),
         "alloc": snapshot["alloc"]
     }
-    with open(path, 'w') as f:
-        json.dump(genesis, f, sort_keys=False, indent=4, separators=(',', ': '))
-    print 'casper genesis generated'
 
+    if path:
+        with open(path, 'w') as f:
+            json.dump(genesis, f, sort_keys=False, indent=4, separators=(',', ': '))
+        print 'casper genesis generated'
+    else:
+        return genesis
+
+
+def usage():
+    print "usage:"
+    print "python pyethapp/tools.py genesis pyethapp/genesisdata/genesis_metropolis.json 3"
+    print "python pyethapp/tools.py datadir 3"
 
 if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        usage()
+        sys.exit(0)
+
     if sys.argv[1] == "genesis":
         generate_genesis(sys.argv[2], int(sys.argv[3]))
+    elif sys.argv[1] == "datadir":
+        generate_data_dirs(int(sys.argv[2]))
     else:
         print "unknown command: %s" % sys.argv[1]
-        print "usage:"
-        print "python pyethapp/tools.py genesis pyethapp/genesisdata/genesis_metropolis.json 3"
+        usage()
         sys.exit(1)
