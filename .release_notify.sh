e2fbea0578d2c17e6ee5c35580ff066b53872bda
Ulrich Petri <ulo@ulo.pe>
2016-10-18 20:09:54 +0200

    Fix Travis PyPI release -> RocketChat notifications

--- .release_notify.sh
+++ .release_notify.sh
@@ -2,5 +2,5 @@
 PAYLOAD="{\"text\": \"<https://pypi.python.org/pypi/pyethapp|pyethapp $TRAVIS_TAG> was released on pypi!\"}"
 curl -s -X POST --data-urlencode "payload=$PAYLOAD" $SLACK_WEBHOOK_URL
 
-PAYLOAD="{\"text\": \"[pyethapp $TRAVIS_TAG](https://pypi.org/project/pyethapp) was released on PyPI!\"}"
+PAYLOAD="{\"attachments\":[{\"text\":\"[pyethapp $TRAVIS_TAG](https://pypi.org/project/pyethapp) was released on PyPI!\",\"color\":\"good\"}]}"
 curl -s -X POST --data-urlencode "payload=$PAYLOAD" $ROCKETCHAT_URL
