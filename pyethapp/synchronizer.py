9a932c2f82ac6fd7db1b4dba0d7bd6e001d940f6
Jan Xie <jan.h.xie@gmail.com>
2016-12-06 10:41:22 +0800

    prevent newblock message triggered sync if already in sync

--- pyethapp/synchronizer.py
+++ pyethapp/synchronizer.py
@@ -329,7 +329,7 @@ class Synchronizer(object):
         if self.chainservice.knows_block(block_hash=t_block.header.prevhash):
             log.debug('adding block')
             self.chainservice.add_block(t_block, proto)
-        else:
+        elif not self.chainservice.is_syncing:
             log.debug('missing parent')
             if not self.synctask:
                 self.synctask = SyncTask(self, proto, t_block.header.hash, chain_difficulty)
