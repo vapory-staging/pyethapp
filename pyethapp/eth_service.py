5aab9930e9dddf2d9e250778302aaba2710c19c4
Jan Xie <jan.h.xie@gmail.com>
2017-05-11 23:23:21 +0800

    set log to debug level

--- pyethapp/eth_service.py
+++ pyethapp/eth_service.py
@@ -409,7 +409,7 @@ class ChainService(WiredService):
         log.debug('status received', proto=proto, eth_version=eth_version)
         assert eth_version == proto.version, (eth_version, proto.version)
         if network_id != self.config['eth'].get('network_id', proto.network_id):
-            log.warn("invalid network id", remote_network_id=network_id,
+            log.debug("invalid network id", remote_network_id=network_id,
                      expected_network_id=self.config['eth'].get('network_id', proto.network_id))
             raise eth_protocol.ETHProtocolError('wrong network_id')
 
